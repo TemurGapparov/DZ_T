bc0365b8ab6ca9217b905bcae0562d87a489e557
Temur <gapparovte@gmail.com>
2023-08-10 14:24:20 +0500

    DZ1

---
+++ DZ1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.InputEncoding = System.Text.Encoding.GetEncoding("utf-16");
+string i = Console.ReadLine();
+int number = Convert.ToInt32(i);
+
+int result = number * number;
+Console.WriteLine("Наш результат: " + result);
