e74b6b0827f54514b57d385c1c75ee1348affdb7
Temur <gapparovte@gmail.com>
2023-08-30 16:49:51 +0500

    DZ6_1

--- DZ6/Program.cs
+++ DZ6/Program.cs
@@ -49,29 +49,44 @@
 // Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
 // [1,0,1,1,0,1,0,0]
 
-void Random(int[] array)
-{
-    Random rnd = new Random();
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = rnd.Next(2);
-    }
-}
+// void Random(int[] array)
+// {
+//     Random rnd = new Random();
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = rnd.Next(2);
+//     }
+// }
 
-// // void PrintArr(int[] array)
-// // {
-// //     for (int i = 0; i < array.Length; i++)
-// //     {
-// //         Console.Write(array[i]+" ");
-// //     }
-// // }
+// // // void PrintArr(int[] array)
+// // // {
+// // //     for (int i = 0; i < array.Length; i++)
+// // //     {
+// // //         Console.Write(array[i]+" ");
+// // //     }
+// // // }
 
-int[] array = new int[8];
+// int[] array = new int[8];
 
-Random(array);
-// // PrintArr(array);
-// // foreach (int i in array)
-// // {
-// //     Console.Write(i + " ");
-// // }
-Console.WriteLine(String.Join(" ", array));
+// Random(array);
+// // // PrintArr(array);
+// // // foreach (int i in array)
+// // // {
+// // //     Console.Write(i + " ");
+// // // }
+// Console.WriteLine(String.Join(" ", array));
+
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+
+
+
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
